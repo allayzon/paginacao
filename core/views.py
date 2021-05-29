94dda45b0375d733c05e979da86d4e0c833d4f7d
Allyson Santos <allyson.g.d.santos@gmail.com>
2021-05-29 11:21:06 -0300

    Initial commit

---
+++ core/views.py
@@ -0,0 +1,11 @@
+from django.views.generic import ListView
+
+from .models import Produto
+
+
+class IndexListView(ListView):
+    template_name = 'index.html'
+    model = Produto
+    paginate_by = 2
+    ordering = 'id'  # Podemos listar por id, nome, -nome(decrescente)
+
